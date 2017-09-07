360f1007ab18b6bbb4cee9367252be2d5021eaf5
taekyunKimDev <powerging2@nate.com>
2017-09-07 17:13:07 +0900

    텝 메뉴&컨텐츠 분리형 (data 어트리뷰트 미사용) 추가

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -1,7 +1,7 @@
 $(function(){
+  var $active = 'active';
   function firstTab(){
-    var $btn = $('.btn-tab');
-    var $active = 'active';
+    var $btn = $('.tabComponent1 .btn-tab');
     $btn.on('click' , function(){
       $(this).closest('li').addClass($active).siblings('li').removeClass($active);
       event.preventDefault();
@@ -10,18 +10,33 @@ $(function(){
   function secondTab(){
     var $btn = $('.tabComponent2 .tab-head li a');
     var $tabCont = $('.tabComponent2 .tab-content');
-    var $active = 'active';
     $btn.on('click' ,function() {
       var tab_id = $(this).attr('data-tab');
       $btn.parent('li').removeClass($active);
       $tabCont.removeClass($active);
       $(this).parent('li').addClass($active);
       $("#" + tab_id).addClass($active);
+      event.preventDefault();
     });
+  }
+
+  function thirdTab(){
+    var $btn = $('.tabComponent3 .tab-head li a');
+    var $tabCont = $('.tabComponent3 .tab-content');
+    $btn.on('click', function(){
+      var index = $btn.index(this);
+      $btn.parent('li').removeClass($active);
+      $(this).closest('li').addClass($active);
+      $tabCont.removeClass($active);
+      $tabCont.eq(index).addClass($active);
+      event.preventDefault();
+    });
+
   }
 
 
 
   firstTab();
   secondTab();
+  thirdTab();
 });
