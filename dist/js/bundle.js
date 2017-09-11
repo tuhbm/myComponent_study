4916c9fa3540cfe9325d89b3f72bbcacc6de5bfc
taekyunKimDev <powerging2@nate.com>
2017-09-11 22:14:47 +0900

    progressbar 추가

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -98,9 +98,37 @@ $(function(){
 
   function accordionUi(){
     $('.accordion').accordion();
-  };
+  }
+
   function datepickerUi(){
     $('.datepicker').datepicker();
+  }
+
+  function progressbarUi(){
+    var progressbar = $( "#progressbar" ),
+      progressLabel = $( ".progress-label" );
+
+    progressbar.progressbar({
+      value: false,
+      change: function() {
+        progressLabel.text( progressbar.progressbar( "value" ) + "%" );
+      },
+      complete: function() {
+        progressLabel.text( "Complete!" );
+      }
+    });
+
+    function progress() {
+      var val = progressbar.progressbar( "value" ) || 0;
+
+      progressbar.progressbar( "value", val + 2 );
+
+      if ( val < 99 ) {
+        setTimeout( progress, 80 );
+      }
+    }
+
+    setTimeout( progress, 2000 );
   }
 
 
@@ -111,4 +139,5 @@ $(function(){
   searchList();
   accordionUi();
   datepickerUi();
+  progressbarUi();
 });
